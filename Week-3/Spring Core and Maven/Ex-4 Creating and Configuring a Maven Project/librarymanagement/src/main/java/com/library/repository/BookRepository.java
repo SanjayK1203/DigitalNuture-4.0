dfbb5b7a460a5d532a123b83166894c55923a868
Sanjay K <sanjaykugan05@gmail.com>
2025-07-05 22:47:20 +0530

    Initial commit with Spring projects

---
+++ Week-3/Spring Core and Maven/Ex-4 Creating and Configuring a Maven Project/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void displayRepositoryInfo() {
+        System.out.println("BookRepository: Repository layer accessed.");
+    }
+}
---
+++ Week-3/Spring Core and Maven/Ex-4 Creating and Configuring a Maven Project/librarymanagement/src/test/java/com/library/librarymanagement/LibrarymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.librarymanagement;
+
+import org.junit.Test;  // <-- JUnit 4 import
+import static org.junit.Assert.assertTrue;
+
+public class LibrarymanagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("Test executed successfully.");
+        assertTrue(true);
+    }
+}
